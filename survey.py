6727babec8133ff0e1b257902789ca7aaadd2cc8
reBiocoder <39401819+reBiocoder@users.noreply.github.com>
2018-07-10 18:39:43 +0800

    Update survey.py

--- survey.py
+++ survey.py
@@ -6,7 +6,7 @@ import  random
 #获取随机值
 while  1:
     my_session=requests.session()
-    header={"referer":"https://www.wjx.cn/jq/25877509.aspx",
+    header={"referer":"https://www.wjx.cn/jq/25877509.aspx"，  #你的问卷星地址
     "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}
     url="https://www.wjx.cn/jq/25877509.aspx"
     res=my_session.get(url,headers=header,verify=False)
